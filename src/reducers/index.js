d2b1943e0a4d43dcaec10c1e25b9ae63e8fcab1b
Leon Brüwer <leon.bruewer@web.de>
2018-09-04 15:03:33 +0200

    migrated add sites formular from local state to global store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import fetchData from './fetchData';
 import siteList from './siteList';
 import searchBar from './searchBar';
 import showMoreButton from './showMoreButton';
+import changeValue from './addSite';
 
 export default combineReducers({
     fetchData,
     siteList,
     searchBar,
-    showMoreButton
+    showMoreButton,
+    changeValue
 });
