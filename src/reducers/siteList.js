64db9f4e70f230a0ceea507a09fcf16a8f4c2ae2
Leon Brüwer <leon.bruewer@web.de>
2018-09-04 12:08:27 +0200

    created action to append list

--- src/reducers/siteList.js
+++ src/reducers/siteList.js
@@ -1,4 +1,4 @@
-import { CREATE_LIST, CLEAR_LIST } from '../actions/siteList';
+import { CREATE_LIST, APPEND_LIST, CLEAR_LIST } from '../actions/siteList';
 
 const initialState = {
     listData: [{
@@ -11,6 +11,14 @@ const siteList = (state = initialState, action) => {
         case CREATE_LIST:
             return {
                 listData: action.data
+            }
+        case APPEND_LIST:
+            let oldData = state.listData;
+            let newData = action.data;
+            let fullData = oldData.concat(newData);
+
+            return {
+                listData: fullData
             }
         case CLEAR_LIST:
             return initialState;
